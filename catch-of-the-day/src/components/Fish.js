6e3275c34eb4f3ccf73f65e9bef0886de3effb9e
hellbertos <james.ruggieri@yahoo.com>
2016-10-28 07:45:39 -0700

    Hook up Add To Order button. Use index attr to pass key to each fish via props.

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -4,6 +4,9 @@ import {formatPrice} from '../helpers';
 class Fish extends React.Component {
 	render() {
 		const details = this.props.details;
+		// ES6 "Destructuring" const {details} = this.props
+		// Can do the same thing for addToOrder index
+		
 		const isAvailable = details.status === 'available';
 		const buttonText = isAvailable ? 'Add To Order' : 'Sold Out';
 		// ES6 "Destructuring" const {details} = this.props
@@ -15,7 +18,7 @@ class Fish extends React.Component {
 					<span className="price">{formatPrice(details.price)}</span>
 				</h3>
 				<p>{details.desc}</p>
-				<button disabled={!isAvailable}>{buttonText}</button>
+				<button onClick={() => this.props.addToOrder(this.props.index)} disabled={!isAvailable}>{buttonText}</button>
 			</li>
 			)
 	}
