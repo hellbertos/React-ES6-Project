eed21f686921a5d9cdd64d6003fd5a5fa48e598d
hellbertos <james.ruggieri@yahoo.com>
2016-10-28 16:14:16 -0700

    Include Re-base for Firebase connection

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyBjxjbhX6g8DfpRZ8kS4SztN5t-Vfku138",
+    authDomain: "da-catch-79339.firebaseapp.com",
+    databaseURL: "https://da-catch-79339.firebaseio.com",
+});
+
+export default base;
